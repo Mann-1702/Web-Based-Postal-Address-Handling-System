1e5418d7f62ac65d6703b6b00d0e3849638c936e
Mann-1702 <mannshah1702@gmail.com>
2025-03-13 00:41:23 -0700

    Added styles in styles.css and updated App.js file.

--- postal-address-ui/src/App.js
+++ postal-address-ui/src/App.js
@@ -4,13 +4,14 @@ import AddressForm from './components/AddressForm';
 import AddressList from './components/AddressList';
 import './styles.css';
 import HomePage from './components/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>Postal Address Handling System</h1>
+        <h1>Postal Address Handling System1</h1>
         <Routes>
           <Route path="/" element={<HomePage />} />
           {/* <Route path="/add" element={<AddressForm />} /> */}
